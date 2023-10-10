b23c3d7ba047a2ee23bf47636c36f7449730281d
lasteik <lastevilday@gmail.com>
2023-10-10 15:32:53 +0500

    Initial commit

---
+++ src/main/java/ru/aston/banktest/BanktestApplication.java
@@ -0,0 +1,13 @@
+package ru.aston.banktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aston/banktest/BanktestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.banktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
